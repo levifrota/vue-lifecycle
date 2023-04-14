a234753773a6d3aadc7a24b0d6cd47460d9fa2cf
Levi Frota <gideao.frota@enext.com.br>
2023-04-14 14:18:47 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,78 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <TheHeader v-if="showHeader" />
+    <h1>Hello World</h1>
+    <br />
+    <input v-model="name" type="text" />
+    {{ name }} <br />
+
+    <button @click="showHeader = !showHeader">Ativar e desativar header</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TheHeader from "@/components/TheHeader";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { TheHeader },
+  data() {
+    return {
+      name: "Jon Snow",
+      showHeader: true,
+    };
+  },
+  // CRIAÇÃO
+  // Preparar o componente
+  // Ajax, inicializar algumas variáveis
+  // Não tem acesso ao template (DOM)
+
+  // MONTAGEM
+  // Inicializar um lib externa (new Lib())
+  // Precisa de acesso ao template (DOM)
+  // Tem acesso ao template (DOM)
+
+  // ATUALIZAÇÃO
+  // Debug
+  // Update
+
+  // DESMONTAGEM
+  // Remover tudo o que for necessário (lib->destroy())
+  // para liberar memória
+
+  // HOOKS
+  beforeCreate() {
+    console.log("beforeCreate");
+    console.log("Estado:", this.name);
+    console.log("DOM:", this.$el);
+  },
+  created() {
+    console.log("created");
+    console.log("Estado:", this.name);
+    console.log("DOM:", this.$el);
+  },
+  beforeMount() {
+    console.log("beforeMount");
+    console.log("Estado:", this.name);
+    console.log("DOM:", this.$el);
+  },
+  mounted() {
+    console.log("mounted");
+    console.log("Estado:", this.name);
+    console.log("DOM:", this.$el);
+  },
+  beforeUpdate() {
+    console.log("beforeUpdate", this.name);
+  },
+  updated() {
+    console.log("updated", this.name);
+  },
+
+  watch: {},
+
+  computed: {},
+
+  methods: {},
+};
 </script>
 
 <style>
@@ -19,8 +80,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px;
 }
 </style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,30 @@
+<template>
+  <header class="header">Header</header>
+</template>
+
+<script>
+export default {
+  beforeUnmount() {
+    // Destruir as nossas libs
+    // Eventos
+    // Listeners
+    console.log("beforeUnmount");
+  },
+  unmounted() {
+    console.log("unmounted");
+  },
+
+  methods: {
+    resize($evt) {
+      console.log($evt);
+    },
+  },
+};
+</script>
+
+<style>
+.header {
+  background: #000;
+  color: #fff;
+}
+</style>
